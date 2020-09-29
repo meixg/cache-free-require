38482a0e51ddbe6781349c9e3924d7fd8d9014d8
meixg <meixg@foxmail.com>
2020-09-29 19:20:57 +0800

    feat: support json and remove filename option

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 const fs = require('fs');
 const Module = require('module');
+const path = require('path');
 
 exports.makeRequire = function(dirname) {
-    return function(moduleName, filename) {
-        return exports.require(moduleName, dirname, filename);
+    return function(moduleName) {
+        return exports.require(moduleName, dirname);
     }
 }
 
-exports.cacheFreeRequire = function(moduleName, dirname, filename = 'temp-file.js') {
+exports.cacheFreeRequire = function(moduleName, dirname) {
     const loaderContext = {context: dirname};
-    const module = new Module(filename, loaderContext);
+    const module = new Module('fake cache free module', loaderContext);
   
     module.paths = Module._nodeModulePaths(loaderContext.context);
-    const path = require.resolve(moduleName, {paths: [loaderContext.context, ...module.paths]});
-    console.log(path);
-    const content = fs.readFileSync(path, 'utf-8');
-    module.filename = filename;
+    const filePath = require.resolve(moduleName, {paths: [loaderContext.context, ...module.paths]});
+    console.log(filePath);
+    const content = fs.readFileSync(filePath, 'utf-8');
+    module.filename = path.basename(filePath);
+    console.log(module.filename);
 
-    // json file is parsed and is assigned to the module exports property
-    if (filename.endsWith('.js')) {
+    if (module.filename.endsWith('.json')) {
+        module.exports = JSON.parse(content);
+    }
+    else {
         module._compile(content, filename);
     }
   
