2685ea9955ce8f41c476ec800046306306a38d72
zhangjingyuan02 <zhangjingyuan02@baidu.com>
2020-09-30 21:28:20 +0800

    fix: in case files has same basename, they influence each other

--- src/index.js
+++ src/index.js
@@ -10,15 +10,19 @@ exports.makeRequire = function(dirname) {
 
 exports.cacheFreeRequire = function(moduleName, dirname) {
     const loaderContext = {context: dirname};
-    const module = new NativeModule(filename, loaderContext);
-  
-    module.paths = NativeModule._nodeModulePaths(loaderContext.context);
-    const path = require.resolve(moduleName, {paths: [loaderContext.context, ...module.paths]});
-    const content = fs.readFileSync(path, 'utf-8');
+    const module = new Module('fake cache free module', loaderContext);
+
+    module.paths = Module._nodeModulePaths(loaderContext.context);
+    const filePath = require.resolve(moduleName, {paths: [loaderContext.context, ...module.paths]});
+    const content = fs.readFileSync(filePath, 'utf-8');
+
+    const filename = filePath;
     module.filename = filename;
 
-    // json file is parsed and is assigned to the module exports property
-    if (filename.endsWith('.js')) {
+    if (module.filename.endsWith('.json')) {
+        module.exports = JSON.parse(content);
+    }
+    else {
         module._compile(content, filename);
     }
   
